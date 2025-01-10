0bb3ce2bc3b2f2641dfc4839faa5fd3e62778b67
Noa.O <noa.obringer@gmail.com>
2025-01-10 10:19:43 +0100

    Co-authored-by: JulienMnt <JulienMnt@users.noreply.github.com>

---
+++ js/script/usercreate.js
@@ -0,0 +1,17 @@
+import createUser from '../supabase/insert.js';
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const firstName = formData.get('firstName');
+    const lastName = formData.get('lastName');
+    const user = await createUser(firstName, lastName, email, password);
+    if (user) {
+        console.log(user);
+    }
+    else {
+        alert('Error creating user');
+    }
+});
